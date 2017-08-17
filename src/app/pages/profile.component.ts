e60c4598f6e835b960e6d534889549493d835196
Jennifer Schroen <jenniferschroen@jennifers-mbp.megacenter.de.ibm.com>
2017-08-17 16:19:05 +0200

    adds education code-by-color #140956

--- src/app/pages/profile.component.ts
+++ src/app/pages/profile.component.ts
@@ -9,7 +9,23 @@ import { Component } from '@angular/core';
 export class ProfileComponent {
 
   /*adds education code-by-color #140956*/
-
+  schools = [
+    {
+      name: 'Generic School',
+      start: new Date('2003-09-01'),
+      end: new Date('2009-07-01'),
+    },
+    {
+      name: 'Generic High School',
+      start: new Date('2009-09-01'),
+      end: new Date('2012-06-01'),
+    },
+    {
+      name: 'Generic Univeristy',
+      start: new Date('2012-09-01'),
+      end: new Date(),
+    }
+  ];
 
   /*adds jobs code-by-color #140956*/
 
