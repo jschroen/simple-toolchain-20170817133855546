d577c7d8a33201378f4734ee0b2b1a76ef4d0faa
Jennifer Schroen <jenniferschroen@jennifers-mbp.megacenter.de.ibm.com>
2017-08-17 16:31:27 +0200

    adds discovery query to frontend code-by-color #AB1A86

--- src/app/pages/learn.component.ts
+++ src/app/pages/learn.component.ts
@@ -22,6 +22,45 @@ export class LearnComponent implements OnInit {
   ) { }
 
   /*adds discovery query to frontend code-by-color #AB1A86*/
+  queryData(requestString: string): void {
+    // check if request string is not empty
+    if(requestString !== "") {
+      this.results = [];
+      this.discoverySpinner = true;
+      console.log(requestString);
+      this.dataService.query(requestString).then(data => {
+        console.log(data);
+        data.results.forEach(result => {
+          result.html = result.html.split('<body>')[1];
+          result.html = result.html.split('</body>')[0];
+  
+          if(result.highlight.hasOwnProperty('html')) {
+            result.highlight.html.forEach(highlight => {
+              // fix split line-break marks
+              highlight = highlight.split(/(?:<br\/>|<br)/g).join('<br/>');
+              // mark highlight paragraphs in document
+              let text = highlight.split('<em>').join('');
+              text = text.split('</em>').join('');
+              let textIndex = result.html.indexOf(text);
+              if (textIndex > 0) {
+                highlight.split('').join('');
+                let html = result.html.slice(0, textIndex) + '<mark>' + highlight + '</mark>' + result.html.slice(textIndex + text.length);
+                result.html = html;
+              }
+              // check highlight paragraphs in document
+              let xss = new RegExp("<(?!br\/>|mark>|\/|p>|em>)");
+              if (xss.test(result.html)) {
+                console.warn(result.html);
+              }
+            })
+          }
+        });
+        // show results in frontend
+        this.discoverySpinner = false;
+        this.results = data.results;
+      });
+    }
+  }
 
   ngOnInit(): void {
     /*adds data-topics code-by-color #3B0256*/
